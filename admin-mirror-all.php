5c7d9da08b623e8ee061e74301088503c16cc6d1
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-04-13 23:10:40 +0930

    Arm the mirror script :)

--- admin-mirror-all.php
+++ admin-mirror-all.php
@@ -5,15 +5,27 @@ $result = str_replace('/', '', $result);
 $packages = preg_split('/[\r\n]+/', $result);
 
 foreach ($packages as $package) {
+    if  ($package == 'installphars') { continue; }
+    if  ($package == 'AllTests.php') { continue; }
+
    $workbench = $package . "-workbench";
    if (!file_exists($workbench)) {
-       print "mkdir ". $workbench . "\n";
-       print "cd " . $workbench . "\n";
-       print "git-svn-mirror init --authors=../authors.txt --from=http://svn.php.net/repository/pear/packages/" . $package . "/ --to=git@github.com:pear/" . $package . ".git\n";
-       print "cd ..\n";
-   } else  {
-       print "cd " . $workbench . "\n";
-       print "git-svn-mirror update \n";
-       print "cd ..\n";
+        $paths = array();
+        $paths[] = "mkdir ". $workbench;
+        $paths[] = "cd " . $workbench;
+        $paths[] = "git-svn-mirror init --authors=../authors.txt --from=http://svn.php.net/repository/pear/packages/" . $package . "/ --to=git@github.com:pear/" . $package . ".git";
+        $paths[] = "cd ..";
+        shell_exec(implode("&&", $paths));
+
+   }
+}
+
+foreach ($packages as $package) {
+    if  ($package == 'installphars') { continue; }
+    if  ($package == 'AllTests.php') { continue; }
+
+   $workbench = $package . "-workbench";
+   if (file_exists($workbench)) {
+       shell_exec("git-svn-mirror update $workbench");
    }
 }
